f39162a61aa40b35d599fb528601f2bb0a674d96
rbverissimo <rverissimo.dev@gmail.com>
2023-01-11 22:59:14 -0300

    criando a lógica da implementação do CampoObservador para os botões gráficos

--- src/br/com/rvrsmo/view/BotaoCampo.java
+++ src/br/com/rvrsmo/view/BotaoCampo.java
@@ -3,12 +3,57 @@ package br.com.rvrsmo.view;
 import javax.swing.JButton;
 
 import br.com.rvrsmo.model.Campo;
+import br.com.rvrsmo.model.CampoEvento;
+import br.com.rvrsmo.model.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
+	
+	/*
+	 * essa classe implementa graficamente as logicas do Campo (Model)
+	 */
 	
 	public BotaoCampo(Campo c) {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch(evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		default:
+			aplicarEstiloPadrao();
+			//Não é necessário criar o desmarcar porque ele volta ao estilo padrão; 
+		}
+		
+	}
+
+	private void aplicarEstiloPadrao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloExplodir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloMarcar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void aplicarEstiloAbrir() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
